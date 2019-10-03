364d7da31f64a782a8b727104064bd5bc35943f0
Lady Trejos <ladytrejos@utp.edu.co>
2019-10-02 21:49:04 -0500

    collectstatic

--- ludiclass/settings.py
+++ ludiclass/settings.py
@@ -162,8 +162,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static'),
-    ('bundles', os.path.join(BASE_DIR, 'reactapp/bundles'))
+    os.path.join(BASE_DIR, 'build/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
