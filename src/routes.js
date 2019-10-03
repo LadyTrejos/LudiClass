1ae3107033700720b946be27d2ab2f84dc7a79ed
Lady Trejos <ladytrejos@utp.edu.co>
2019-10-02 19:04:11 -0500

    remove login and register route

--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { PrivateRoute } from './components/PrivateRoute';
 
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
 import Home from './components/Home';
 import Index from './containers/Index';
-import Activity from './components/CreateActivity';
 
 
 const BaseRouter = () => (
     <div>
       <Switch>
-        <Route exact path="/login" component={LoginForm}/>
-        <Route exact path='/registro' component={RegisterForm}/>
         <Route exact path='/index' component={Index}/>
         <PrivateRoute path="/" component={Home}/>
       </Switch>
