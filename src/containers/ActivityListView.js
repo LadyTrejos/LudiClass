4cb7614c5d4a9ea9585d0c15c063ded6aa6168f6
GermanGrandas <gertiven@gmail.com>
2019-11-20 22:45:26 -0500

    cambios sobre activityListView

--- src/containers/ActivityListView.js
+++ src/containers/ActivityListView.js
@@ -1,112 +1,156 @@
-import React from 'react';
-import axios from 'axios';
-import { Row, Input,Col, Button } from 'antd';
-import ViewActivity from '../components/ViewActivity'
-import HOSTNAME from '../helpers/hostname';
-import Styles from './ActivityListView.module.css';
-import { async } from 'q';
+import React from "react";
+import axios from "axios";
+import { Row, Input, Col, Button } from "antd";
+import ViewActivity from "../components/ViewActivity";
+import HOSTNAME from "../helpers/hostname";
+import Styles from "./ActivityListView.module.css";
 
 const Search = Input.Search;
 
 class ActivityListView extends React.Component {
-state={
-    activity:'',
+  state = {
+    activity: "",
     events: [],
-    favorites:[ ]
-}
+    favorites: [],
+    filtered: []
+  };
 
-componentDidMount(){
-    this.loadData()
-}
+  componentDidMount() {
+    const { filter } = this.props;
+    if (filter === "all") {
+      axios
+        .get(`${HOSTNAME}/api/activity/?ordering=-created_at`)
+        .then(({ data }) => {
+          this.setState({
+            activity: data,
+            filtered: data
+          });
+        });
+    } else {
+      axios
+        .get(`${HOSTNAME}/api/activity/?search=${filter}&&ordering=-created_at`)
+        .then(res => {
+          this.setState({
+            activity: res.data,
+            filtered: res.data
+          });
+        });
+    }
 
-componentWillMount(){
-    const userID = localStorage.getItem('user');
+    /*try {
+      
+    } catch (e) {
+      console.log("no data");
+    }*/
+  }
+  componentWillReceiveProps(nextProps) {
+    const { filter } = nextProps;
+    console.log(filter);
+    if (filter === "all") {
+      axios
+        .get(`${HOSTNAME}/api/activity/?ordering=-created_at`)
+        .then(({ data }) => {
+          this.setState({
+            filtered: data
+          });
+        });
+    } else {
+      axios
+        .get(`${HOSTNAME}/api/activity/?search=${filter}&&ordering=-created_at`)
+        .then(res => {
+          this.setState({
+            filtered: res.data
+          });
+        });
+    }
+  }
+  /*
+  
+  componentWillMount() {
+    const userID = localStorage.getItem("user");
 
-    axios.get(`${HOSTNAME}/api/users/${userID}/`)
-    .then(res => {
-        this.setState({
+    axios.get(`${HOSTNAME}/api/users/${userID}/`).then(res => {
+      this.setState({
         favorites: res.data.activities
-        })
-    })
+      });
+    });
+  }*/
 
-}
-
-loadData = () => {
-    axios.get(`${HOSTNAME}/api/activity/?ordering=-created_at`)
-    .then(res =>{
+  handleSearch = value => {
+    const { activity } = this.state;
+    let newActi = [];
+    if (value !== "") {
+      newActi = activity.filter(item =>
+        item.name.toLowerCase().includes(value.toLowerCase())
+      );
+      this.setState({
+        filtered: newActi
+      });
+    } else {
+      this.setState({
+        filtered: activity
+      });
+    }
+    /*axios
+      .get(`${HOSTNAME}/api/activity/?search=${value}&&ordering=-created_at`)
+      .then(res => {
+        console.log("res: ", res);
         this.setState({
-            activity: res.data
-        })
-    })
+          activity: res.data
+        });
+      });*/
+  };
 
-}
+  handleSearchFavorites = value => {
+    let activities = [],
+      promises = [];
 
-handleSearch = (value) => {
-    axios.get(`${HOSTNAME}/api/activity/?search=${value}&&ordering=-created_at`)
-    .then(res =>{
-        console.log('res: ',res)
-        this.setState({
-            activity: res.data
-        })
-    })
-}
-
-handleSearchFavorites = (value) =>{
-    let activities = [], promises = [];
-    
     for (const prop in value) {
-        console.log(`value.${prop} = ${value[prop]}`);
-        promises.push(axios.get(`${HOSTNAME}/api/activity/${value[prop]}/`))
-      }
+      console.log(`value.${prop} = ${value[prop]}`);
+      promises.push(axios.get(`${HOSTNAME}/api/activity/${value[prop]}/`));
+    }
 
-      axios.all(promises)
-      .then(results => {
-        results.forEach(item => activities.push(item.data))
-        this.setState({
-            activity: activities
-         })
-      }
-      )
-    
-    
+    axios.all(promises).then(results => {
+      results.forEach(item => activities.push(item.data));
+      this.setState({
+        activity: activities
+      });
+    });
+  };
 
+  render() {
+    const { filtered } = this.state;
 
-}
+    return (
+      <div>
+        <Row type="flex" justify="center" align="middle">
+          <h2 className={Styles.h2}>Actividades</h2>
+        </Row>
+        <Row>
+          <Col sm={12} md={16} lg={18} xl={18} xxl={18}>
+            <Search
+              placeholder="Buscar actividad"
+              onSearch={value => this.handleSearch(value)}
+              enterButton
+              size="large"
+              style={{ maxWidth: 300 }}
+            />
+          </Col>
+          <Col sm={12} md={8} lg={6} xl={6} xxl={6}>
+            <Button
+              onClick={() => this.handleSearchFavorites(this.state.favorites)}
+              className={Styles.button}
+            >
+              Mis actividades favoritas
+            </Button>
+          </Col>
+        </Row>
 
-render(){
-    return(
-        <div>
-            <Row type="flex" justify="center" align="middle">
-                <h2 className={Styles.h2}>Actividades</h2>
-            </Row>
-            <Row >
-                <Col  sm={12} md={16} lg={18} xl={18} xxl={18}>
-                <Search
-                    placeholder="Buscar actividad"
-                    onSearch={value => this.handleSearch(value)}
-                    enterButton
-                    size='large'
-                    style={{maxWidth: 300}}
-                />
-                </Col>
-                <Col  sm={12} md={8} lg={6} xl={6} xxl={6}>
-                    <Button 
-                    onClick={() => this.handleSearchFavorites(this.state.favorites)}
-                    className={Styles.button}
-                    >
-                        Mis actividades favoritas
-                    </Button>
-                </Col>
-                
-
-            </Row>
-
-            <br/>
-            <ViewActivity data={this.state.activity} loadData={this.props.data}/>
-        </div>
-    )
-}
-
+        <br />
+        <ViewActivity data={filtered} loadData={this.props.data} />
+      </div>
+    );
+  }
 }
 
 export default ActivityListView;
--- src/containers/UserLayout.js
+++ src/containers/UserLayout.js
@@ -1,49 +1,61 @@
-import React from 'react';
-import { Layout, Menu, Button, Row, Col, Input } from 'antd';
-import { connect } from 'react-redux';
-import { withRouter, Link } from 'react-router-dom';
-import * as actions from '../store/actions/auth';
-import axios from 'axios';
-import HOSTNAME from '../helpers/hostname';
-import ActivityListView from './ActivityListView';
-import styles from './UserLayout.module.css';
+import React from "react";
+import { Layout, Menu, Button, Row, Col, Input } from "antd";
+import { connect } from "react-redux";
+import { withRouter, Link } from "react-router-dom";
+import * as actions from "../store/actions/auth";
+import axios from "axios";
+import HOSTNAME from "../helpers/hostname";
+import ActivityListView from "./ActivityListView";
+import styles from "./UserLayout.module.css";
 
 const { Header, Content, Footer, Sider } = Layout;
 const { Search } = Input;
 
 class UserLayout extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-        username: "",
-        activity:'',
+      username: "",
+      actFiltered: "all"
     };
   }
 
-  componentDidMount() {
-    const userID = localStorage.getItem('user');
-    axios.get(`${HOSTNAME}/api/users/${userID}/`)
-    .then(res => {
-        this.setState({
-            username: res.data.username
-        })
-    })
+  async componentDidMount() {
+    try {
+      const userID = localStorage.getItem("user");
+      let { username } = await axios
+        .get(`${HOSTNAME}/api/users/${userID}/`)
+        .then(res => res.data);
 
-    axios.get(`${HOSTNAME}/api/activity/?ordering=-created_at`)
-      .then(res =>{
-          this.setState({
-              activity: res.data
-          })
-      })
-  
-}
-
-  render(){
-    return(
-      <Layout >
+      this.setState({
+        username
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  searchSubject = value => {
+    if (value !== "") {
+      this.setState({
+        actFiltered: value
+      });
+    } else {
+      this.setState({
+        actFiltered: "all"
+      });
+    }
+  };
+  render() {
+    const { actFiltered } = this.state;
+    return (
+      <Layout>
         <Sider
-          style={{backgroundColor: '#241190', flex:1, justifyContent:'flex-end', alignContent:'left'}}
+          style={{
+            backgroundColor: "#241190",
+            flex: 1,
+            justifyContent: "flex-end",
+            alignContent: "left"
+          }}
           breakpoint="lg"
           collapsedWidth="0"
           onBreakpoint={broken => {
@@ -52,37 +64,48 @@ class UserLayout extends React.Component {
           onCollapse={(collapsed, type) => {
             console.log(collapsed, type);
           }}
-        >         
-          
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['7']} style={{backgroundColor:'#241190', justifyContent: 'center'}}>
+        >
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["7"]}
+            style={{ backgroundColor: "#241190", justifyContent: "center" }}
+          >
             {/* <h2 className={styles.menutitle}>Menú</h2> */}
             <a href="/index">
-              <img className={styles.brand} src={require('../static/img/brand_logo.png')} alt="Logo de LudiClass"/>
+              <img
+                className={styles.brand}
+                src={require("../static/img/brand_logo.png")}
+                alt="Logo de LudiClass"
+              />
             </a>
             {/* <div><Divider style={{backgroundColor:'white'}}/></div> */}
             <Menu.Item key="1">
               <span className={styles.option}>Crear actividad</span>
-              <Link to="/createActivity"/>
+              <Link to="/createActivity" />
             </Menu.Item>
 
             <Menu.Item key="2">
               <span className={styles.option}>Ver actividades</span>
-              <Link to="activityListView"/>
+              <Link to="activityListView" />
             </Menu.Item>
           </Menu>
-        <Button
+          <Button
             type="primary"
             onClick={this.props.logout}
-            className={styles.logout}>
+            className={styles.logout}
+          >
             Cerrar sesión
-        </Button>
+          </Button>
         </Sider>
-        
-        <Layout style={{backgroundColor: 'rgba(36, 17, 144, 0.2)'}}>
-          <Header className={styles.header} >
+
+        <Layout style={{ backgroundColor: "rgba(36, 17, 144, 0.2)" }}>
+          <Header className={styles.header}>
             <Row type="flex">
               <Col xs={0} sm={0} md={10} lg={10} xl={10}>
-                <h3 className={styles.username}>{this.state.username.toLowerCase()}</h3>
+                <h3 className={styles.username}>
+                  {this.state.username.toLowerCase()}
+                </h3>
               </Col>
               <Col xs={23} sm={24} md={11} lg={9} xl={9}>
                 <Search
@@ -90,21 +113,31 @@ class UserLayout extends React.Component {
                   enterButton="Buscar"
                   size="large"
                   className={styles.searchbar}
-                  onSearch={value => console.log(value)}
+                  onSearch={value => this.searchSubject(value)}
                 />
               </Col>
             </Row>
           </Header>
-          <Content style={{ margin: '24px 16px'}}>
-            <div style={{ padding: '3vh 2vw', minHeight: '82vh', backgroundColor:'white', borderRadius: '10px'}}>
-                {this.props.location.pathname === '/' ?<ActivityListView data={this.state.activity} loadData={this.loadData}/>:this.props.children}
+          <Content style={{ margin: "24px 16px" }}>
+            <div
+              style={{
+                padding: "3vh 2vw",
+                minHeight: "82vh",
+                backgroundColor: "white",
+                borderRadius: "10px"
+              }}
+            >
+              {this.props.location.pathname === "/" ? (
+                <ActivityListView filter={actFiltered} />
+              ) : (
+                this.props.children
+              )}
             </div>
-            
           </Content>
           {/* <Footer style={{ textAlign: 'center' }}>@LudiClass</Footer> */}
         </Layout>
       </Layout>
-  );
+    );
   }
 }
 
@@ -112,13 +145,15 @@ const mapStateToProps = state => {
   return {
     loading: state.loading,
     error: state.error
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
     logout: () => dispatch(actions.logout())
-  }
-}
+  };
+};
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(UserLayout));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(UserLayout)
+);
